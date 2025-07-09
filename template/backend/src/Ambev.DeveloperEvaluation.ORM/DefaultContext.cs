262066f8a06d2756f950c6de671f3a6610b1d2d2
zilmarcristiano <68968121+zilmarcristiano@users.noreply.github.com>
2025-07-09 13:57:07 -0300

    Initial commit

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/DefaultContext.cs
@@ -0,0 +1,42 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace Ambev.DeveloperEvaluation.ORM;
+
+public class DefaultContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public DefaultContext(DbContextOptions<DefaultContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(modelBuilder);
+    }
+}
+public class YourDbContextFactory : IDesignTimeDbContextFactory<DefaultContext>
+{
+    public DefaultContext CreateDbContext(string[] args)
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<DefaultContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        builder.UseNpgsql(
+               connectionString,
+               b => b.MigrationsAssembly("Ambev.DeveloperEvaluation.WebApi")
+        );
+
+        return new DefaultContext(builder.Options);
+    }
+}
